054915c797b9aa95d662b14f83dda8443c9b95d5
Remco <SteamingMutt@users.noreply.github.com>
2016-10-22 16:56:13 +0200

    ༼ つ ◕_◕ ༽つ (#8)
    * ༼ つ ◕_◕ ༽つ
    
    * ༼ つ ◕_◕ ༽つ

--- src/lib/numbers.js
+++ src/lib/numbers.js
@@ -13,10 +13,10 @@ function generateQueue () {
 
   for (var i = 0; i < 10; i++) {
     output.push({
-      title: new Buffer(String(Math.random()*100)).toString('base64'),
-      length: Math.floor(Math.random()*1000),
+      title: new Buffer(String(Math.random() * 100)).toString('base64'),
+      length: Math.floor(Math.random() * 1000),
       service: 'ShortwaveRadio',
-      identifier: new Buffer(String(Math.random()*100)).toString('base64'),
+      identifier: new Buffer(String(Math.random() * 100)).toString('base64'),
       social: {
         likes: 0,
         dislikes: 0
@@ -39,8 +39,12 @@ exports.run = function (db) {
       client.Channels.get(process.env['BOT_LOGCHANNEL']).sendMessage('Let us begin the ritual, my children. 👀')
 
       client.Guilds.get(process.env['BOT_GUILDID']).voiceChannels.forEach(chan => {
-        channels.push({id: chan.id, position: chan.position, name: chan.name})
-        chan.update(new Buffer(String(Math.random()*100)).toString('base64'))
+        channels.push({
+          id: chan.id,
+          position: chan.position,
+          name: chan.name
+        })
+        chan.update(new Buffer(String(Math.random() * 100)).toString('base64'))
       })
 
       client.Guilds.get(process.env['BOT_GUILDID']).createChannel('voice', '???').then(vChan => {
@@ -67,41 +71,46 @@ exports.run = function (db) {
               if (!err) {
                 client.Guilds.get(process.env['BOT_GUILDID']).edit(null, icon, null, undefined, null, null).catch(err => logger.error('Failed to set main guild icon: ' + err))
               }
-              setTimeout(setIcon, 3*60*1000)
+              setTimeout(setIcon, 3 * 60 * 1000)
             })
           }
           setIcon()
 
           // Set database info every 10s
           let now = new Date()
+
           function setDb () {
             if (stop) return
 
-            db.get('queue').update({bot: '???'}, {
+            db.get('queue').update({
+              bot: '???'
+            }, {
               $set: {
                 bot: '???',
                 playStart: now,
                 queue: generateQueue(),
                 current: {
-                  title: new Buffer(String(Math.random()*100)).toString('base64'),
-                  length: Math.floor(Math.random()*1000),
+                  title: new Buffer(String(Math.random() * 100)).toString('base64'),
+                  length: Math.floor(Math.random() * 1000),
                   service: 'ShortwaveRadio',
-                  identifier: new Buffer(String(Math.random()*100)).toString('base64'),
+                  identifier: new Buffer(String(Math.random() * 100)).toString('base64'),
                   social: {
                     likes: 0,
                     dislikes: 0
                   }
                 }
               }
-            }, { upsert: true }).catch(err => logger.error('Failed to set db queue info: ' + err))
+            }, {
+              upsert: true
+            }).catch(err => logger.error('Failed to set db queue info: ' + err))
 
-            setTimeout(setDb, 30*1000)
+            setTimeout(setDb, 30 * 1000)
           }
           setDb()
 
           let pool = fs.readdirSync(path.join(__dirname, '..', 'audio'))
           let played = []
-          let finish = new Date((new Date()).getTime() + (5*60*1000)) // 5 minutes
+          let finish = new Date((new Date()).getTime() + (5 * 60 * 1000)) // 5 minutes
 
           // Loop playing until we've passed the finish time
           function loopPlay () {
@@ -120,14 +129,16 @@ exports.run = function (db) {
                 }
               })
 
-              db.get('queue').remove({bot: '???'}).catch(err => logger.error('Failed to remove queue information from db: ' + err))
+              db.get('queue').remove({
+                bot: '???'
+              }).catch(err => logger.error('Failed to remove queue information from db: ' + err))
 
               let members = v.voiceConnection.channel.members
               var count = members.length
 
               if (client.Guilds.get(process.env['BOT_GUILDID']).afk_channel_id !== null) {
                 for (var i = 0; i < members.length; i++) {
-                  if (members[i].id != client.User.id && client.Guilds.get(process.env['BOT_GUILDID']).afk_channel_id) members[i].setChannel(client.Guilds.get(process.env['BOT_GUILDID']).afk_channel_id).catch(err => logger.error('Failed to move user to AFK: ' + err))
+                  if (members[i].id !== client.User.id && client.Guilds.get(process.env['BOT_GUILDID']).afk_channel_id) members[i].setChannel(client.Guilds.get(process.env['BOT_GUILDID']).afk_channel_id).catch(err => logger.error('Failed to move user to AFK: ' + err))
                 }
               }
 
@@ -144,15 +155,17 @@ exports.run = function (db) {
                       'Content-Type': 'application/json',
                       'Content-Length': Buffer.byteLength(JSON.stringify(channels).length)
                     }
-                  }, () => { resolve(count) })
- 
+                  }, () => {
+                    resolve(count)
+                  })
+
                   req.on('error', (e) => {
                     logger.error(`Problem resetting channel positions: ${e.message}`)
                   })
- 
+
                   req.write(JSON.stringify(channels))
                   req.end()
-        
+
                   client.Channels.get(process.env['BOT_LOGCHANNEL']).sendMessage('End of transmission. Enlightened ' + (count - 1) + ' disciples.\nPlayed ' + played.join(', ') + '.')
                   resolve(count)
                   client.disconnect()
@@ -164,7 +177,7 @@ exports.run = function (db) {
               return
             }
 
-            var i = Math.floor(Math.random() * pool.length)
+            i = Math.floor(Math.random() * pool.length)
             var track = pool[i]
             played.push(track)
             pool.splice(i, 1)
@@ -184,6 +197,8 @@ exports.run = function (db) {
     })
 
     // Do it for the vine
-    client.connect({ token: process.env['DISCORD_NUMBERTOKEN'] })
+    client.connect({
+      token: process.env['DISCORD_NUMBERTOKEN']
+    })
   })
 }
